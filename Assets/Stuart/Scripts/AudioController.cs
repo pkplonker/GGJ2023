19ced6891d294a8bcc17b3d7c1cbc80372f70ffe
stuart <pkplonker@gmail.com>
2023-02-04 23:57:48 +0000

    audio controller

---
+++ Assets/Stuart/Scripts/AudioController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+	public static AudioController instance { get; private set; }
+	[SerializeField] private AudioClip audioClip;
+	private AudioSource source;
+
+	private void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(this);
+	}
+
+	public void PlaySound(AudioClip clip)
+	{
+		if (clip == null) clip = audioClip;
+		source.PlayOneShot(clip);
+	}
+}
