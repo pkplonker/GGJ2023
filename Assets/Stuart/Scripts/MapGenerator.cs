3b96694509ea8e64ae1a1f6aefe4ed704143a4bd
Stuart Heath <heaths8@uni.coventry.ac.uk>
2023-02-04 08:42:51 +0000

    map scaling

--- Assets/Stuart/Scripts/MapGenerator.cs
+++ Assets/Stuart/Scripts/MapGenerator.cs
@@ -6,7 +6,7 @@ namespace Stuart
 {
     public class MapGenerator : MonoBehaviour
     {
-        [field: SerializeField] public Vector2 sizeScale { get; private set; } = new(1, 1);
+        [field: SerializeField] public float sizeScale { get; private set; } = 1f;
         [SerializeField] private Camera camera;
         [SerializeField] private Vector3 startOrientation;
         private GameObject plane;
@@ -16,26 +16,30 @@ namespace Stuart
 
         private GameObject frame;
         private GameObject background;
-
+        private float defaultCameraSize;
         private void Awake()
         {
             if (camera == null) camera = Camera.main;
+            defaultCameraSize = camera.orthographicSize;
         }
 
-        void Start() => GenerateMap(sizeScale);
+        void Start() => GenerateMap();
 
-        private void GenerateMap(Vector2 size)
+        private void GenerateMap()
         {
             GenerateDirt();
             GenerateFrame();
             SetCameraStartPosition();
         }
 
-        private void SetCameraStartPosition()
+        private void Update()
         {
-            //set camera start point
+            if (Input.GetKeyDown(KeyCode.R)) GenerateMap();
         }
+    
 
+        private void SetCameraStartPosition()=>camera.orthographicSize =defaultCameraSize* sizeScale;
+        
         private void GenerateFrame()
         {
             if (frame != null) Destroy(frame);
@@ -44,21 +48,22 @@ namespace Stuart
             background = GeneratePlane(backgroundMaterial);
         }
 
-        private GameObject GeneratePlane(Material material)
+        private GameObject GeneratePlane(Material material, bool scale= false)
         {
             var go = GameObject.CreatePrimitive(PrimitiveType.Plane);
-            go.transform.localScale = new Vector3(sizeScale.x, sizeScale.y, 1);
+            go.transform.localScale = new Vector3(sizeScale, 1,sizeScale);
             go.transform.rotation = Quaternion.Euler(startOrientation);
             var mr = go.GetComponent<MeshRenderer>();
             mr.material = material;
-            material.mainTextureScale *= sizeScale;
+            if(scale)
+                material.mainTextureScale *= sizeScale;
             return go;
         }
 
         private void GenerateDirt()
         {
             if (plane != null) Destroy(plane);
-            plane = GeneratePlane(dirtMaterial);
+            plane = GeneratePlane(dirtMaterial,true);
         }
     }
 }
