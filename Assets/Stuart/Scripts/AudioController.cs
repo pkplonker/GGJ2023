86dbd6db30dbaa07a842886d051646cfffee04ef
Stuart Heath <heaths8@uni.coventry.ac.uk>
2023-02-05 15:28:33 +0000

    namespace

--- Assets/Stuart/Scripts/AudioController.cs
+++ Assets/Stuart/Scripts/AudioController.cs
@@ -3,25 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-public class AudioController : MonoBehaviour
+namespace Stuart
 {
-    public static AudioController instance { get; private set; }
-    [SerializeField] private AudioClip audioClip;
-    private AudioSource source;
-
-    private void Awake()
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioController : MonoBehaviour
     {
-        if (instance == null)
-            instance = this;
-        else if (instance != this)
-            Destroy(this);
-        source = GetComponent<AudioSource>();
-    }
+        public static AudioController instance { get; private set; }
+        [SerializeField] private AudioClip audioClip;
+        private AudioSource source;
 
-    public void PlaySound(AudioClip clip)
-    {
-        if (clip == null) clip = audioClip;
-        source.PlayOneShot(clip);
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(this);
+            source = GetComponent<AudioSource>();
+        }
+
+        public void PlaySound(AudioClip clip)
+        {
+            if (clip == null) clip = audioClip;
+            source.PlayOneShot(clip);
+        }
     }
 }
