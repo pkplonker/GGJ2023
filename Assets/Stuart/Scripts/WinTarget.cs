1c488c7a6767c7ddc10edb67af480d9cfb9d23ef
Stuart Heath <heaths8@uni.coventry.ac.uk>
2023-02-05 09:17:27 +0000

    single winner

--- Assets/Stuart/Scripts/WinTarget.cs
+++ Assets/Stuart/Scripts/WinTarget.cs
@@ -9,11 +9,13 @@ namespace Stuart
     public class WinTarget : MonoBehaviour
     {
         public static event Action<int> OnPlayerWin;
-
+        private int winner = -1;
         private void OnTriggerEnter(Collider other)
         {
+            if (winner!=-1) return;
             var invent = other.GetComponent<Inventory>();
             if (invent == null) return;
+            winner = invent.playerId;
             Debug.Log($"Winner is Player {invent.playerId}");
             OnPlayerWin?.Invoke(invent.playerId);
         }
