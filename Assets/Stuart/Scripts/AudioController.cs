59ff4ab920dfc13ea138f9ba6fd2ad7b283c78b7
Stuart Heath <heaths8@uni.coventry.ac.uk>
2023-02-05 09:05:25 +0000

    null fix

--- Assets/Stuart/Scripts/AudioController.cs
+++ Assets/Stuart/Scripts/AudioController.cs
@@ -6,21 +6,22 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class AudioController : MonoBehaviour
 {
-	public static AudioController instance { get; private set; }
-	[SerializeField] private AudioClip audioClip;
-	private AudioSource source;
+    public static AudioController instance { get; private set; }
+    [SerializeField] private AudioClip audioClip;
+    private AudioSource source;
 
-	private void Awake()
-	{
-		if (instance == null)
-			instance = this;
-		else if (instance != this)
-			Destroy(this);
-	}
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this);
+        source = GetComponent<AudioSource>();
+    }
 
-	public void PlaySound(AudioClip clip)
-	{
-		if (clip == null) clip = audioClip;
-		source.PlayOneShot(clip);
-	}
+    public void PlaySound(AudioClip clip)
+    {
+        if (clip == null) clip = audioClip;
+        source.PlayOneShot(clip);
+    }
 }
